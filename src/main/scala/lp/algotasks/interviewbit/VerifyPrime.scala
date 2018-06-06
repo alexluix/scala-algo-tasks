473aa04014a6426a828dc171092b2cc57303943e
alukianchuk <alukianchuk@aquivalabs.com>
2018-06-07 00:43:42 +0300

    task solution: Verify Prime – optimized with Sieve of Eratosthenes algorithm

--- src/main/scala/lp/algotasks/interviewbit/VerifyPrime.scala
+++ src/main/scala/lp/algotasks/interviewbit/VerifyPrime.scala
@@ -11,11 +11,20 @@ object VerifyPrime {
     if (A <= 1) return 0
     if (A == 2) return 1
 
-    for (i <- 2 to Math.sqrt(A).ceil.toInt) {
-      if (A % i == 0) return 0
+    val primesIdx = Array.fill(A + 1)(true)
+    val sqrtA = Math.sqrt(A).toInt
+
+    for (i <- 2 to sqrtA) {
+      if (primesIdx(i)) {
+        for (j <- 2 to (A / i)) {
+          val idx = i * j
+          if (idx == A) return 0
+          primesIdx(idx) = false
+        }
+      }
     }
 
-    1
+    if (primesIdx(A)) 1 else 0
   }
 
 }
--- src/test/scala/lp/algotasks/interviewbit/VerifyPrimeSpec.scala
+++ src/test/scala/lp/algotasks/interviewbit/VerifyPrimeSpec.scala
@@ -22,8 +22,20 @@ class VerifyPrimeSpec extends FlatSpec {
     assert(subject.isPrime(6) === 0)
   }
 
+  it should "find out that 73 is prime" in {
+    assert(subject.isPrime(73) === 1)
+  }
+
   it should "find out that 84923 is not prime" in {
     assert(subject.isPrime(84923) === 0)
+  }
+
+  it should "find out that 16777214 is not prime" in {
+    assert(subject.isPrime(16777214) === 0)
+  }
+
+  it should "find out that large number 1300141 is prime" in {
+    assert(subject.isPrime(1300141) === 1)
   }
 
   it should "find out that 31 is prime" in {
